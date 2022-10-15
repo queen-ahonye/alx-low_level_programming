8c69247323fa0f02c647ef4ddd6e3d79a36c6044
queen-ahonye <queenelizabethahonye@gmail>
2022-10-15 16:06:06 -0700

    Variables if else why

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ *main - prints the alphabet in lowercase,
+ *Description - main value
+ *followed by new line
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
