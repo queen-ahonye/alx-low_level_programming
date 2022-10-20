560711bf4ee4575a4cb20b3313a3f020fe7f2dce
queen-ahonye <queenelizabethahonye@gmail>
2022-10-20 06:17:05 -0700

    Fumvtions, Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
-
 /**
  *main - Entry begins
  *Return: nothing, return void
  */
 
+
 int main(void)
 
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_Putchar('c');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
